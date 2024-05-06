5f96eb9b8ee9e70b0778839b49bf447b82171d57
iannjau <lantanacreations@gmail.com>
2024-05-06 20:19:14 +0300

    add my

--- src/App.js
+++ src/App.js
@@ -1,15 +1,3 @@
-import AddPlant from './components/AddPlant';
-import Logo from './components/Logo';
-import './App.css';
 
-function App() {
-  return (   <>
-   
-    <Logo />;
-    <AddPlant />
-    </>);
-}
-
-export default App;
  
   
