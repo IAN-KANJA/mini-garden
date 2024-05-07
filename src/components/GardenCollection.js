0edeb6f0faaad8a081affb9ba3c1549d55271ce9
jasonokingo <jason.okingo@student.moringaschool.com>
2024-05-07 12:21:50 +0300

    Feat:Updated PlantCard

--- src/components/GardenCollection.js
+++ src/components/GardenCollection.js
@@ -1,37 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import GardenCard from './PlantCard';
 
 const GardenCollection = () => {
-    const [plants, setPlants] = useState([]);
-
-    useEffect(() => {
-      fetchPlants();
-    }, []);
-  
-    const fetchPlants = async () => {
-      try {
-        const response = await fetch('db.json');
-        const jsonData = await response.json();
-        setPlants(jsonData.bots);
-      } catch (error) {
-        console.error('Error fetching data: ', error);
-      }
-    };
+   
 
   return (
     <div>
       <h1>Garden-Collection</h1>
-      <ul>
-        {plants.map(plant => (
-          <li key={plant.id}>
-            <img src={plant.image} alt={plant.name} />
-            <p>{plant.name}</p>
-            <p>${plant.price}</p>
-          </li>
-        ))}
-      </ul>
+      <GardenCard />
+      <GardenCard />
+      <GardenCard />
+      <GardenCard />
+      <GardenCard />
+      
     </div>
   );
 };
  export default GardenCollection;
-
-
