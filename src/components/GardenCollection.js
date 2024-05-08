f8001b6ea6b8eea49cbb5296cceec0479a81dc50
iannjau <lantanacreations@gmail.com>
2024-05-08 09:38:22 +0300

    add

--- src/components/GardenCollection.js
+++ src/components/GardenCollection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import GardenCard from './PlantCard';
+
 
 
 const GardenCollection = () => {
@@ -8,13 +8,12 @@ const GardenCollection = () => {
   return (
     <div>
       <h1>Garden-Collection</h1>
-      <GardenCard />
-      <GardenCard />
-      <GardenCard />
-      <GardenCard />
-      <GardenCard />
+      
       
     </div>
   );
 };
  export default GardenCollection;
+
+
+
