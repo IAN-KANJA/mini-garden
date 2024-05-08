55d4539d85d5501d63b426f576f402666c63cb1c
iannjau <lantanacreations@gmail.com>
2024-05-08 10:15:50 +0300

    add

--- src/components/GardenCollection.js
+++ src/components/GardenCollection.js
@@ -7,10 +7,70 @@ const GardenCollection = () => {
 
   return (
     <div>
-      <h1>Garden-Collection</h1>
-      
-      
-    </div>
+		<div className='plants'>
+			<img src='https://media.istockphoto.com/id/1057094656/photo/aloe-vera-plant-isolated-on-white-background-clipping-path-included.jpg?s=612x612&w=0&k=20&c=ZmuCzEw15kCY0ByKaKCfNk6vqx-lSSHD2t2SbgUkGXU='
+			alt='' ></img>
+		</div>
+		<div>
+			<h1>Aloe</h1>
+			<h1>Price: 15:99</h1>
+            <button>Instock</button>
+		</div>
+		<div>
+			<img src='https://thumbs.dreamstime.com/b/zz-plant-elegant-flowerpot-isolated-white-210095822.jpg'
+			alt='' ></img>
+		</div>
+		<div>
+			<h1>ZZ Plant</h1>
+			<h1>Price: 25:98</h1>
+            <button>Instock</button>
+		</div>
+		<div>
+			<img src='https://media.istockphoto.com/id/1263431303/photo/pilea-peperomioides-house-plant-in-a-gray-pot-in-front-of-a-white-wall-chinese-money-plant.jpg?s=612x612&w=0&k=20&c=VK313TkBnkseM_I6nOK2Em2L0WVREKjc6AOWmDCZpSg='
+			alt='' ></img>
+		</div>
+		<div>
+			<h1>Pilea peperomioides</h1>
+			<h1>Price:5:99</h1>
+            <button>Instock</button>
+		</div>
+		<div>
+			<img src='https://media.istockphoto.com/id/1320269359/photo/tropical-epipremnum-aureum-marble-queen-pothos-houseplant-in-flower-pot.jpg?s=612x612&w=0&k=20&c=Rc4J3wkUQgd9vN_O7c7wRnbqCy1UUafqRSYLE2KGv_c='
+			alt='' ></img>
+		</div>
+		<div>
+			<h1>Pothos</h1>
+			<h1>Price: 12:11</h1>
+            <button>Instock</button>
+		</div>
+		<div>
+			<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjqa5Briq2aSSKm64YGO8VyqX3qEI-94TbTiVCjbc-Xw&s'
+			alt='' ></img>
+		</div>
+		<div>
+			<h1>Jade</h1>
+			<h1>Price: 10:37</h1>
+            <button>Instock</button>
+		</div>
+		<div>
+			<img src='https://www.shutterstock.com/image-photo/monstera-deliciosa-swiss-cheese-plant-260nw-2247254857.jpg'
+			alt='' ></img>
+		</div>
+		<div>
+			<h1>Monstera Deliciosa</h1>
+			<h1>Price: 25:99</h1>
+            <button>Instock</button>
+		</div>
+		<div>
+			<img src='https://media.istockphoto.com/id/1299094421/photo/ficus-lirata-in-wickr-pot-on-wooden-table.jpg?s=612x612&w=0&k=20&c=62uI2GfPl4GyTMynYlsLdjAcaY_FvAuiqhIZNUSOLO8='
+			alt='' ></img>
+		</div>
+		<div>
+			<h1>Fiddle Leaf Fig</h1>
+			<h1>Price: 55</h1>
+            <button>Instock</button>
+		</div>
+	</div>
   );
 };
  export default GardenCollection;
