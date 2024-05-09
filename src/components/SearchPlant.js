3af8bde63d8fa2d932b376455aaa632ff6645d22
{Anwar <anwarelghazi8596@gmail.com>
2024-05-09 14:52:32 +0200

    done

--- src/components/SearchPlant.js
+++ src/components/SearchPlant.js
@@ -1,18 +1,16 @@
-import React from "react";
-function Search({search,setsearch}) {
-  return (
-    <div className="searchbar">
-      <label htmlFor="search">Search Plants:</label>
-      <input
-        type="text"
-        id="search"
-        placeholder="Type a name to search..."
-        value ={search}
-        onChange={(e) => setsearch(e.target.value)}
-      />
-    </div>
+const Search = () => {
+    return (
+        <div className="searchbar">
+            <label htmlFor="search">Search Plants:</label>
+            <input
+                type="text"
+                id="search"
+                placeholder="Type a name to search..."
+                onChange={(e) => console.log("Searching...")}
+            />
+        </div>
+    );
 
-  );
-}
+};
 
-export default Search;
+export d
